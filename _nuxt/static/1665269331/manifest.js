e487250e347471ef9edccdbc0fd5ca9dabfca704
Firenzo <firenzo_akkefietje@msn.com>
2022-10-09 15:47:15 +0200

    menu button hide and show on scroll

--- _nuxt/static/1665269331/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fhaircuts","\u002F","\u002Fprivacy-verklaring","\u002Fdisclaimer","\u002Fcontact","\u002Fbehandelingen-en-prijzen"]})
---
+++ _nuxt/static/1665323081/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fdisclaimer","\u002Fprivacy-verklaring","\u002Fhaircuts","\u002Fcontact","\u002Fbehandelingen-en-prijzen","\u002F"]})
